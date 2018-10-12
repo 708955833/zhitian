798c346075a64c548df38608746ed786c6ef7a40
xiujianying <708955833@qq.com>
2018-10-12 11:37:57 +0800

    1

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -24,7 +24,7 @@ use yii\data\Pagination;
 class SiteController extends Controller
 {
     public $root = '/app/frontend/web/';
-    public $imgpath = '/backend/web/';
+    public $imgpath = '/';
 //    public $imgpath = '/ihouse/backend/web/';
     public $keywords = '泰国房产,艾皓思';
     public $description = '';
